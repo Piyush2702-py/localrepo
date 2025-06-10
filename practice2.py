71a63cd762053f0f62136da0a945a9ab4f9a2f74
Piyush2702-py <piyushkumargupta480@gmail.com>
2025-06-10 12:22:14 +0545

    prac2

--- practice2.py
+++ practice2.py
@@ -4,4 +4,13 @@ class Student:
     def __init__(self,name,marks):
         self.name=name
         self.marks=marks 
+        
+    def name(name):
+        name=input("Enter your name:")
+        print(name)
+    
+    def marks(marks):
+        marks=int(input("Enter your marks:"))
 
+Student.name("Piyush")
+print("Hello")

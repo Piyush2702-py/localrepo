0c51514efb542354c1d3941ac1d47e6ae6cfb90b
Piyush2702-py <piyushkumargupta480@gmail.com>
2025-06-18 09:46:24 +0545

    18 June

---
+++ 18th June.py
@@ -0,0 +1,6 @@
+str=input("Enter a letter:")
+if(str=='a' or str=='e' or str=='i' or str=='o' or str=='u'):
+    print("Entered letter is a vowel !")
+else:
+    print("Entered letter is a consonant")
+    

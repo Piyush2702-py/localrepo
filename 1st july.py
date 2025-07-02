e92997cbc2d5b0e5441bb2f231b1666f6e7a5334
Piyush2702-py <piyushkumargupta480@gmail.com>
2025-07-02 08:46:59 +0545

    Student

--- 1st july.py
+++ 1st july.py
@@ -1,5 +1,7 @@
 class Student:
-    def name_1(str):
+    def name_1(str,int):
         print(str)
+        print("Age-",int)
+        print("Roll no.",int)
 
-Student.name_1("piyush")
+Student.name_1("piyush",19)

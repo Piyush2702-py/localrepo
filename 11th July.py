dee7627508f382884e99ae94e3395c46be5f5103
Piyush2702-py <piyushkumargupta480@gmail.com>
2025-07-11 09:44:02 +0545

    gender

---
+++ 11th July.py
@@ -0,0 +1,9 @@
+name=input("What is your name ?")
+gender=input("What is your gender ? (M,F)")
+if gender=='M':
+    print("Hello Mr.",name)
+elif gender=='F':
+    print("Hello Mrs.",name)
+else:
+    print("Invalid input")
+    

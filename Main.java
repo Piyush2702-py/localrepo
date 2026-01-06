dc6bf9f74b9afa2e5dce56cc31d79c3ddaf70bf5
Piyush2702-py <piyushkumargupta480@gmail.com>
2026-01-06 08:26:35 +0530

    Main file

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Rectangle
+{
+    int length;
+    int breadth;
+
+    void calculate()
+    {
+        System.out.println("The area of the recatangle is:"+(length*breadth));
+    }
+}
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        Rectangle box1=new Rectangle();
+
+        
+    }
+}

2d27243c80044633ab694daace7008272a0330cb
Piyush2702-py <piyushkumargupta480@gmail.com>
2026-01-10 09:55:54 +0530

    Sum of two numbers in java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class Hello
 {
     public static void main(String args[])
     {
         System.out.println("Java Programming Language");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int first=sc.nextInt();
+        System.out.println("Enter second number: ");
+        int second=sc.nextInt();
+        int sum=first+second;
+        System.out.println("The sum of "+first+" and "+second+" is: "+sum);
+
     }
 }

7210e87d3b333b27aa49e16b3a4b72abce71d5e4
Piyush2702-py <piyushkumargupta480@gmail.com>
2026-01-18 12:40:19 +0530

    18th Jan

--- Java.java
+++ Java.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+class Hello
+{
+    String name;
+
+    void print()
+    {
+        System.out.println("Hello "+name);
+    }
+}
 public class Java
 {
     public static void main(String args[])
@@ -6,5 +16,11 @@ public class Java
         System.out.println("14th Jan 2026");
         System.out.println("16th Jan 2026");
         System.out.println("17th Jan Saturday 2026");
+
+        Hello h1=new Hello();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        h1.name=sc.nextLine();
+        h1.print();
     }
 }

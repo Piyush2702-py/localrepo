4deeb69f8b3d2b10f07830bd856295ff5737f20b
Piyush2702-py <piyushkumargupta480@gmail.com>
2026-01-24 13:19:27 +0530

    Java Programs

---
+++ _24th_Jan.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class _24th_Jan {
+    public static void main(String args[]){
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name_e=sc.nextLine();
+
+        System.out.println("Enter your roll no:");
+        int roll=sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Enter your section:");
+        String section=sc.nextLine();
+
+        System.out.println("Enter your branch:");
+        String branch=sc.nextLine();
+
+        System.out.println("Student information:");
+        System.out.println("Name:"+name_e);
+        System.out.println("Roll no:"+roll);
+        System.out.println("Section:"+section);
+        System.out.println("Branch:"+branch);
+    }
+}

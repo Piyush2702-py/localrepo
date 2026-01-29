3cd19fb1b03593fd66dac2ddb73fa57283c001f5
Piyush2702-py <piyushkumargupta480@gmail.com>
2026-01-29 09:57:17 +0530

    A java program to count objects

---
+++ _2nd_Aug.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class _2nd_Aug
+{
+static int count = 0;
+	
+_2nd_Aug(){
+count++;
+}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number of objects to create: ");
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++){
+			new _2nd_Aug();
+		}
+		System.out.println("No of objects = " + _2nd_Aug.count);
+		sc.close();
+	}
+}

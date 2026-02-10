9f63114d32824d52f64746edcf9ef110b5256c29
Piyush2702-py <piyushkumargupta480@gmail.com>
2026-02-11 01:04:43 +0530

    A program for account balance

---
+++ _4th_Sept.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+class Account {
+    int acc_no;
+    double balance;
+
+    void input(Scanner sc) {
+        System.out.print("Enter Account Number: ");
+        acc_no = sc.nextInt();
+
+        System.out.print("Enter Balance: ");
+        balance = sc.nextDouble();
+    }
+
+    void disp() {
+        System.out.println("Account Number: " + acc_no);
+        System.out.println("Balance: " + balance);
+    }
+}
+
+class Person extends Account {
+    String name;
+    long aadhar_no;
+
+    void input(Scanner sc) {
+        System.out.print("Enter Name: ");
+        sc.nextLine();   
+        name = sc.nextLine();
+
+        System.out.print("Enter Aadhar Number: ");
+        aadhar_no = sc.nextLong();
+
+        super.input(sc);  
+    }
+
+    void disp() {
+        System.out.println("Name: " + name);
+        System.out.println("Aadhar Number: " + aadhar_no);
+        System.out.println("Account Number: " + acc_no);
+        System.out.println("Balance: " + balance);
+    }
+}
+
+public class _4th_Sept{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Person[] p = new Person[3];
+
+        System.out.println("Enter details of three persons:");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\nPerson " + (i + 1) + ":");
+            p[i] = new Person();
+            p[i].input(sc);
+        }
+
+        System.out.println("\nDisplay details of three persons:");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\nPerson " + (i + 1) + ":");
+            p[i].disp();
+        }
+    }
+}

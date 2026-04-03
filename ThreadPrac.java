3537e128f7764774367fd3070231390e9520a8a4
Piyush2702-py <piyushkumargupta480@gmail.com>
2026-04-03 17:12:16 +0530

    A java program to practice threading

---
+++ ThreadPrac.java
@@ -0,0 +1,16 @@
+class MyThread extends Thread {
+    public void run() {
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch(Exception e) {}
+        }
+    }
+}
+
+public class ThreadPrac{
+    public static void main(String[] args) {
+        new MyThread().start();
+    }
+}

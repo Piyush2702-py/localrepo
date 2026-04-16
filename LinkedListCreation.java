c74b6286bdfa913c327d15f44561e08e8b34e910
Piyush2702-py <piyushkumargupta480@gmail.com>
2026-04-16 22:29:23 +0530

    A java program to create n number of linked list entered by a user

---
+++ LinkedListCreation.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedListCreation{
+    Node head;
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedListExample list = new LinkedListExample();
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+            list.insert(value);
+        }
+
+        list.display();
+
+        sc.close();
+    }
+}

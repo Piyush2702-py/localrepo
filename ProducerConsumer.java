8a8659e4c601e49c517dadd9126d85b7f3bb3a77
Piyush2702-py <piyushkumargupta480@gmail.com>
2026-04-27 23:18:10 +0545

    A java program for producer consumer problem

---
+++ ProducerConsumer.java
@@ -0,0 +1,76 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class SharedBuffer {
+    private Queue<Integer> queue = new LinkedList<>();
+    private int capacity;
+
+    public SharedBuffer(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public synchronized void produce(int value) throws InterruptedException {
+        while (queue.size() == capacity) {
+            wait();
+        }
+        queue.add(value);
+        System.out.println("Produced: " + value);
+        notify();
+    }
+
+    public synchronized void consume() throws InterruptedException {
+        while (queue.isEmpty()) {
+            wait();
+        }
+        int value = queue.poll();
+        System.out.println("Consumed: " + value);
+        notify();
+    }
+}
+
+class Producer extends Thread {
+    private SharedBuffer buffer;
+
+    public Producer(SharedBuffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        int value = 0;
+        try {
+            while (true) {
+                buffer.produce(value++);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Consumer extends Thread {
+    private SharedBuffer buffer;
+
+    public Consumer(SharedBuffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                buffer.consume();
+                Thread.sleep(800);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class ProducerConsumer {
+    public static void main(String[] args) {
+        SharedBuffer buffer = new SharedBuffer(5);
+        new Producer(buffer).start();
+        new Consumer(buffer).start();
+    }
+}
